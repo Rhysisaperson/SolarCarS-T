06eee7efe354de0503615e5cfbe4a7187d861131
WillTRM <willtrm111@gmail.com>
2024-10-08 15:12:24 -0500

    begin attempts to center everything also converted to grid

--- dashboard/src/main.rs
+++ dashboard/src/main.rs
@@ -6,7 +6,7 @@ use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use config::read::read_config;
 use glib::ControlFlow;
-use gtk4::{self as gtk, DrawingArea};
+use gtk4::{self as gtk, DrawingArea, Grid};
 use gtk::prelude::*;
 use gtk::{glib, Application, ApplicationWindow};
 use speedometer::lib::on_draw;
@@ -22,14 +22,34 @@ fn main() -> glib::ExitCode {
             .application(app)
             .default_width(800)
             .default_height(800)
+            .halign(gtk::Align::Center)
+            .valign(gtk::Align::Center)
             .title("Dashboard")
             .build();
 
         // Show the window.
-        let canvas = DrawingArea::default();
+        let grid = Grid::builder()
+        .hexpand(true)
+        .vexpand(true)
+        .build();
 
         let config = read_config();
         let config = Arc::new(config.speedometer.to_decimal());
+
+        // width_request and height_request need to be set to config values
+        let canvas = DrawingArea::builder()
+        .width_request(800)
+        .height_request(800)
+        .build();
+        grid.attach(&canvas, 1, 1, 1, 1);
+
+        let button_1 = gtk::Button::with_label("Left");
+        button_1.connect_clicked(move |_| println!("TURN LEFT !!!!!"));
+        grid.attach(&button_1, 0, 2, 1, 1);
+        let button_2 = gtk::Button::with_label("Right");
+        button_1.connect_clicked(move |_| println!("TURN RIGHT !!!!"));
+        grid.attach(&button_2, 2, 2, 1, 1);
+        
 
         //tf2 coconut
         let speed = Arc::new(Mutex::new(0.5));
@@ -38,7 +58,7 @@ fn main() -> glib::ExitCode {
         canvas.set_draw_func(move |_, cr, _, _| {
             on_draw(cr, config.clone(), speed.clone());
         });
-        window.set_child(Some(&canvas));
+        window.set_child(Some(&grid));
         glib::source::timeout_add_local(Duration::from_millis(10), move| | {
             interval(&canvas, speed2.clone())
         });
